9569c582660fdf56d601f82ce4fbce34703596f4
Andrea Miglietta <m.andrea96@hotmail.it>
2022-08-28 23:38:55 +0200

    bonus 1

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
 console.log('JS ok!!');
 
 // SELECT id FILE HTML
-const containerHTMLElement = document.getElementById('container')
+const containerHTMLElement = document.getElementById('container');
 
 // CYCLING NUMBERS FROM 1 TO 100
-for (let number = 1; number <= 100; number = number + 1) {
+for (let number = 1; number <= 100; number++) {
 
     // PRINT CONTENT CONSOLE
     let text = number;
@@ -16,5 +16,21 @@ for (let number = 1; number <= 100; number = number + 1) {
     } else if (number % 5 === 0) {                  // if rest of division (nr/5) is 0 text --> 'Buzz'
         text = 'Buzz';
     }
+
+    // PRINT NUMBER IF TEXT HASNT CONTENT
+    const result = text.length > 0 ? text : number;
+    console.log(result);
+
+    const element = document.createElement('div');
+
+    element.className = 'square';
+    if (text.length > 0) {
+        element.classList.add(text);
+    }
+
+    element.append(result);
+
+    containerHTMLElement.append(element);
+
 }
 
